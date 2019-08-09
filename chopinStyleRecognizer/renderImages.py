0b7aac4b68f9519432083ffb0a1cee4480cf47eb
Wayne Chen <chen.wayne@outlook.com>
2019-08-09 15:05:27 -0700

    adding chopin style recognizer

---
+++ chopinStyleRecognizer/renderImages.py
@@ -0,0 +1,7 @@
+from PyQt5 import QtWidgets, QtGui, QtCore
+import sys
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = QtWidgets.QMainWindow()
+    app.exec_()
